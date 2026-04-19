70d13b6975b39d0ad28663f6c5a620dd7465fe81
wfmoptiforce-beep <wfmoptiforce@gmail.com>
2026-04-19 00:56:16 +0000

    Add Supabase config

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://vagnzratboqdnstaaaav.supabase.co";
+const SUPABASE_KEY = "sb_publishable_HgFbVWm9Q_g61PJ07da-rQ_XblKrNGE";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
