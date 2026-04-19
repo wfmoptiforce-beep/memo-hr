f380685f61e142b4f7b500f5178d060a70d4e57a
wfmoptiforce-beep <wfmoptiforce@gmail.com>
2026-04-19 01:34:43 +0000

    Fix supabase config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-const SUPABASE_URL = "https://vagnzratboqdnstaaaav.supabase.co";
-const SUPABASE_KEY = "sb_publishable_HgFbVWm9Q_g61PJ07da-rQ_XblKrNGE";
+const { createClient } = supabase;
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const _supabase = createClient(
+  'https://vagnzratboqdnstaaaav.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZ256cmF0Ym9xZG5zdGFhYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NDQxMjAsImV4cCI6MjA5MjEyMDEyMH0.1OkkYQzsXFHqF3TC2nAtfI8GUXO3kVUc0sXbD4CgRMk'
+);
+
+window.supabase = _supabase;
